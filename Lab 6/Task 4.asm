a0d573b1a6bf8f2826273155d707943ce8872acd
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2025-03-13 12:34:33 +0500

    Update Task 4.asm

--- Lab 6/Task 4.asm
+++ Lab 6/Task 4.asm
@@ -1,1 +1,29 @@
+INCLUDE Irvine32.inc
+
+.data
+    Source db "Hello, World!", 0   
+    Target db 256 dup(0)            
+
+.code
+main PROC
+    mov esi, OFFSET Source        
+    mov edi, OFFSET Target       
+    xor ecx, ecx                  
+
+copy:
+    mov al, [esi + ecx]         
+    mov [edi + ecx], al          
+    cmp al, 0                      
+    je done                       
+    inc ecx                       
+    jmp copy                
+
+done:
+    lea edx, Target                
+    call WriteString              
+
+    exit                           
+main ENDP
+
+END main
 
